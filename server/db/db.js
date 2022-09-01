a36bf563974e8f53a8a679b471e0aaff46d5c9a4
Jeff McNie <jeff.mcnie@gmail.com>
2022-09-01 15:24:55 +1200

    Build a select db query

--- server/db/db.js
+++ server/db/db.js
@@ -2,4 +2,10 @@ const config = require('./knexfile').development
 // eslint-disable-next-line no-unused-vars
 const connection = require('knex')(config)
 
-module.exports = {}
+async function getTheEntries(db = connection) {
+    return db('entries').select('id', 'song_link AS songLink', 'song_text AS songText', 'has_image AS hasImages', 'image', 'image_text AS imageText' )
+} 
+
+module.exports = {
+  getTheEntries,
+}
